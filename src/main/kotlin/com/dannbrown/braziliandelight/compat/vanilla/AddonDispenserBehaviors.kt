2aea1734108406405b7bc5b4073ef7ba20343ca0
Daniel Brown <danielbrown25.9.2@gmail.com>
2024-05-08 10:17:09 -0300

    coconut dispenser behavior

--- src/main/kotlin/com/dannbrown/braziliandelight/compat/vanilla/AddonDispenserBehaviors.kt
+++ src/main/kotlin/com/dannbrown/braziliandelight/compat/vanilla/AddonDispenserBehaviors.kt
@@ -1,6 +1,9 @@
 package com.dannbrown.braziliandelight.compat.vanilla
 
+import com.dannbrown.braziliandelight.content.entity.CoconutProjectileEntity
+import com.dannbrown.braziliandelight.init.AddonBlocks
 import com.dannbrown.databoxlib.registry.datagen.DataboxDispenserBehaviors
 
 object AddonDispenserBehaviors: DataboxDispenserBehaviors() {
+    val COCONUT_DISPENSER = registerThrowableBehavior({ AddonBlocks.COCONUT.get() }, CoconutProjectileEntity::class)
 }
