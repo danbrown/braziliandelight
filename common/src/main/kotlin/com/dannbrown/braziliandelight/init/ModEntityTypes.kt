ba2f28f7ed72e04daf5ab6e312a0c7ce39d42b57
Daniel Brown <danielbrown25.9.2@gmail.com>
2025-03-28 17:16:25 -0300

    fix

--- common/src/main/kotlin/com/dannbrown/braziliandelight/init/ModEntityTypes.kt
+++ common/src/main/kotlin/com/dannbrown/braziliandelight/init/ModEntityTypes.kt
@@ -6,6 +6,7 @@ import com.dannbrown.braziliandelight.init.ModContent.REGISTRATE
 import com.dannbrown.deltaboxlib.content.item.arrow.BaseArrowRenderer
 import net.minecraft.client.renderer.entity.ThrownItemRenderer
 import net.minecraft.world.entity.MobCategory
+import java.util.function.Function
 
 object ModEntityTypes {
   val REPUGNANT_ARROW = REGISTRATE.entityType<RepugnantArrow>(ModNames.REPUGNANT_ARROW)
@@ -17,7 +18,7 @@ object ModEntityTypes {
         .clientTrackingRange(4)
         .updateInterval(20)
     }
-    .renderer { ctx -> BaseArrowRenderer(ctx, ModContent.MOD_ID, ModNames.REPUGNANT_ARROW) }
+    .renderer { Function { ctx -> BaseArrowRenderer(ctx, ModContent.MOD_ID, ModNames.REPUGNANT_ARROW) } }
     .register()
 
   val COCONUT_PROJECTILE = REGISTRATE.entityType<CoconutProjectileEntity>(ModNames.COCONUT)
@@ -26,7 +27,7 @@ object ModEntityTypes {
     .properties { p ->
       p.sized(0.5f, 0.5f)
     }
-    .renderer { ctx -> ThrownItemRenderer(ctx) }
+    .renderer { Function { ctx -> ThrownItemRenderer(ctx) } }
     .register()
 
   fun register() {
